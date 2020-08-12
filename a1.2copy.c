673de2ca001d24a6d8a6ac9aaf78b72574c2dc06
etinaude <etinaude@gmail.com>
2020-08-13 11:33:06 +1200

    added files

---
+++ a1.2copy.c
@@ -0,0 +1,160 @@
+/*
+    The sorting program to use for Operating Systems Assignment 1 2020
+    written by Robert Sheehan
+
+    Modified by: Etienne Naude
+    UPI: enau831
+
+    By submitting a program you are claiming that you and only you have made
+    adjustments and additions to this code.
+
+    clear;cc   -O2   a1.1.c -o   1 -pthread;time ./1 10
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <stdbool.h>
+#include <sys/times.h>
+#include <pthread.h>
+
+#define SIZE 10
+
+struct block
+{
+    int size;
+    int *data;
+};
+
+void print_data(struct block my_data)
+{
+    for (int i = 0; i < my_data.size; ++i)
+        printf("%d ", my_data.data[i]);
+    printf("\n");
+}
+
+/* Split the shared array around the pivot, return pivot index. */
+int split_on_pivot(struct block my_data)
+{
+    int right = my_data.size - 1;
+    int left = 0;
+    int pivot = my_data.data[right];
+    while (left < right)
+    {
+        int value = my_data.data[right - 1];
+        if (value > pivot)
+        {
+            my_data.data[right--] = value;
+        }
+        else
+        {
+            my_data.data[right - 1] = my_data.data[left];
+            my_data.data[left++] = value;
+        }
+    }
+    my_data.data[right] = pivot;
+    return right;
+}
+
+void *pass_on(void *data)
+{
+    struct block *block = data;
+    quick_sort(*block);
+}
+/* Quick sort the data. */
+void quick_sort(struct block my_data)
+{
+    if (my_data.size < 2)
+        return;
+    int pivot_pos = split_on_pivot(my_data);
+
+    struct block left_side, right_side;
+
+    left_side.size = pivot_pos;
+    left_side.data = my_data.data;
+    right_side.size = my_data.size - pivot_pos - 1;
+    right_side.data = my_data.data + pivot_pos + 1;
+
+    /*printf("right:%d %d\n", right_side.size, *right_side.data);
+    printf("left: %d %d\n", left_side.size, *left_side.data);
+    if (pthread_create(&ptid, NULL, quick_sort, &left_side))
+    {
+        fprintf(stderr, "Error creating thread\n");
+    }
+    else
+    {
+        printf("..\n");
+    }
+    quick_sort(right_side);
+    pthread_join(ptid, NULL);
+    printf(",,\n");*/
+    pthread_t ptid;
+    pthread_create(&ptid, NULL, &pass_on, (void *)&right_side);
+    quick_sort(left_side);
+    pthread_join(ptid, NULL);
+}
+
+/* Check to see if the data is sorted. */
+bool is_sorted(struct block my_data)
+{
+    bool sorted = true;
+    for (int i = 0; i < my_data.size - 1; i++)
+    {
+        if (my_data.data[i] > my_data.data[i + 1])
+            sorted = false;
+    }
+    return sorted;
+}
+
+/* Fill the array with random data. */
+void produce_random_data(struct block my_data)
+{
+    srand(1); // the same random data seed every time
+    for (int i = 0; i < my_data.size; i++)
+    {
+        my_data.data[i] = rand() % 1000;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    long size;
+
+    if (argc < 2)
+    {
+        size = SIZE;
+    }
+    else
+    {
+        size = atol(argv[1]);
+    }
+    struct block start_block;
+    start_block.size = size;
+    start_block.data = (int *)calloc(size, sizeof(int));
+    if (start_block.data == NULL)
+    {
+        printf("Problem allocating memory.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    produce_random_data(start_block);
+
+    if (start_block.size < 1001)
+        print_data(start_block);
+
+    struct tms start_times, finish_times;
+    times(&start_times);
+    printf("start time in clock ticks: %ld\n", start_times.tms_utime);
+    quick_sort(start_block);
+    times(&finish_times);
+    printf("finish time in clock ticks: %ld\n", finish_times.tms_utime);
+
+    if (start_block.size < 1001)
+        print_data(start_block);
+
+    printf(is_sorted(start_block) ? "sorted\n" : "not sorted\n");
+    free(start_block.data);
+    exit(EXIT_SUCCESS);
+}
---
+++ results.md
@@ -0,0 +1,33 @@
+# Results
+
+## Info
+
+- Name: Etienne Naude
+- UPI: enau831
+- ID: 768485633
+- Computer specs
+  - OS: Kubuntu 20.04 (run natively)
+  - Model: ThinkPad E14
+  - 16 Threads, 8 core
+  - CPU: i5-10210u
+  - RAM: 8GB
+  - CC version: 9.3.0
+
+| Step | input size | 1       | 2       | 3       | Average |
+| ---- | ---------- | ------- | ------- | ------- | ------- |
+| 1    | 10,000,000 | 78.818s | 79.046s | 79.733s | 79.199s |
+| 2?   | 10,000,000 | 73.446s | 72.191s | 71.376s | 72.338s |
+| #2.5 | 10,000,000 | 9.377s  | 8.579s  | 12.443s | 10.133s |
+| 3    |            |         |         |         |         |
+| 4    |            |         |         |         |         |
+| 5    |            |         |         |         |         |
+| 6    |            |         |         |         |         |
+| 7    |            |         |         |         |         |
+
+## Step 2
+
+This is slightly quicker but not substantially so
+
+## Step 2.5
+
+I dont think this is what they wanted
