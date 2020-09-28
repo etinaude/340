26767a3d0bfc91066f94cc9b49a17d4010bfde04
Etienne Naude <etinaude@gmail.com>
2020-09-28 16:06:09 +1300

    methods py

--- A2/methods.py
+++ A2/methods.py
@@ -14,16 +14,11 @@ def chmod(self, path, mode):
 
     ~~this changes the mode of the file by using the and operation with the octal number 770000 then the or operation with the mode~
 
-
-
 def chown(self, path, uid, gid):
     self.files[path]['st_uid'] = uid
     self.files[path]['st_gid'] = gid
-
 ?
     ~~this changes the owner of a file by setting the uid and gid~
-
-
 
 def create(self, path, mode):
     self.files[path] = dict(st_mode=(S_IFREG | mode), st_nlink=1,
@@ -50,19 +45,11 @@ def getxattr(self, path, name, position=0):
 ?
     ~~this returns a specific attribute of a file in the dictionary rather than all attributes~
 
-
-
-
-
-
 def listxattr(self, path):
     attrs = self.files[path].get('attrs', {})
     return attrs.keys()
 
     ~~this returns the names of all the attributes of a file in the dictionary~
-
-
-
 
 def mkdir(self, path, mode):
     self.files[path] = dict(st_mode=(S_IFDIR | mode), st_nlink=2,
@@ -76,22 +63,22 @@ def open(self, path, flags):
     self.fd += 1
     return self.fd
 
-
+    ~~ ~
 
 def read(self, path, size, offset, fh):
     return self.data[path][offset:offset + size]
 
-
+    ~~this reads data from a file starting at the offset and ending at offset+length~
 
 def readdir(self, path, fh):
     return ['.', '..'] + [x[1:] for x in self.files if x != '/']
-
-
+?
+    ~~this reads the contents of a directory ~
 
 def readlink(self, path):
     return self.data[path]
-
-
+?
+    ~~returns the class stored at the path in the dictionary~
 
 def removexattr(self, path, name):
     attrs = self.files[path].get('attrs', {})
@@ -100,48 +87,47 @@ def removexattr(self, path, name):
     except KeyError:
         pass        # Should return ENOATTR
 
-
+    ~~this removes and attribute from a file by deleting the attribute name from the file in the dictionary~
 
 def rename(self, old, new):
     self.files[new] = self.files.pop(old)
 
-
+    ~~this renames a file by remapping the item from the dictionary to a new name and removing it from the old location~
 
 def rmdir(self, path):
     self.files.pop(path)
     self.files['/']['st_nlink'] -= 1
-
-
+?
+    ~~this deletes a directory by first removing it from the dictionary and the reducing the the st_nlink count by 1~
 
 def setxattr(self, path, name, value, options, position=0):
     # Ignore options
     attrs = self.files[path].setdefault('attrs', {})
     attrs[name] = value
 
-
+    ~~ this sets an attribute of a specific file by retreiving the file attributes form the dictionary and adding the specified value to the attribute~
 
 def statfs(self, path):
     return dict(f_bsize=512, f_blocks=4096, f_bavail=2048)
 
 
-    
 def symlink(self, target, source):
     self.files[target] = dict(st_mode=(S_IFLNK | 0o777), st_nlink=1,
                               st_size=len(source))
     self.data[target] = source
 
-
+    ~~this creates a link between a target and a source by copying the data from the source to the target~
 
 def truncate(self, path, length, fh=None):
     self.data[path] = self.data[path][:length]
     self.files[path]['st_size'] = length
 
-
+    ~~this shortens a file by deleting all content after the specified length and sets the length of the file to be shorter~
 
 def unlink(self, path):
     self.files.pop(path)
 
-
+    ~~this simply removes an item from the dictionary~
 
 def utimens(self, path, times=None):
     now = time()
@@ -149,9 +135,11 @@ def utimens(self, path, times=None):
     self.files[path]['st_atime'] = atime
     self.files[path]['st_mtime'] = mtime
 
+    ~~this sets the a and m time attributes of the file to the current time if the parameter times was not set ~
 
-    
 def write(self, path, data, offset, fh):
     self.data[path] = self.data[path][:offset] + data
     self.files[path]['st_size'] = len(self.data[path])
     return len(data)
+
+    ~~ this appends a file by writing extra data to it, it then also updates the length of the file and returns the length of the new data~
