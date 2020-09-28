d4ae297529bc2675ed42dfa50a58de48eed77bc0
Etienne Naude <etinaude@gmail.com>
2020-09-28 15:16:32 +1300

    part 1

---
+++ A2/methods.py
@@ -0,0 +1,157 @@
+def __init__(self):
+        self.files = {}
+        self.data = defaultdict(bytes)
+        self.fd = 0
+        now = time()
+        self.files['/'] = dict(st_mode=(S_IFDIR | 0o755), st_ctime=now,
+                               st_mtime=now, st_atime=now, st_nlink=2)
+
+
+def chmod(self, path, mode):
+    self.files[path]['st_mode'] &= 0o770000
+    self.files[path]['st_mode'] |= mode
+    return 0
+
+    ~~this changes the mode of the file by using the and operation with the octal number 770000 then the or operation with the mode~
+
+
+
+def chown(self, path, uid, gid):
+    self.files[path]['st_uid'] = uid
+    self.files[path]['st_gid'] = gid
+
+?
+    ~~this changes the owner of a file by setting the uid and gid~
+
+
+
+def create(self, path, mode):
+    self.files[path] = dict(st_mode=(S_IFREG | mode), st_nlink=1,
+                            st_size=0, st_ctime=time(), st_mtime=time(),
+                            st_atime=time())
+    self.fd += 1
+    return self.fd
+
+    ~~this creates a new file by adding it to the dictionary with the specific path and setting its attributes~
+
+def getattr(self, path, fh=None):
+    if path not in self.files:
+        raise FuseOSError(ENOENT)
+    return self.files[path]
+
+    ~~this returns an item from the files dictionary if it exists else it raises an error~ 
+
+def getxattr(self, path, name, position=0):
+    attrs = self.files[path].get('attrs', {})
+    try:
+        return attrs[name]
+    except KeyError:
+        return ''       # Should return ENOATTR
+?
+    ~~this returns a specific attribute of a file in the dictionary rather than all attributes~
+
+
+
+
+
+
+def listxattr(self, path):
+    attrs = self.files[path].get('attrs', {})
+    return attrs.keys()
+
+    ~~this returns the names of all the attributes of a file in the dictionary~
+
+
+
+
+def mkdir(self, path, mode):
+    self.files[path] = dict(st_mode=(S_IFDIR | mode), st_nlink=2,
+                            st_size=0, st_ctime=time(), st_mtime=time(),
+                            st_atime=time())
+    self.files['/']['st_nlink'] += 1
+
+    ~~this creates a new directory at a specific path by adding it to the dictionary~
+
+def open(self, path, flags):
+    self.fd += 1
+    return self.fd
+
+
+
+def read(self, path, size, offset, fh):
+    return self.data[path][offset:offset + size]
+
+
+
+def readdir(self, path, fh):
+    return ['.', '..'] + [x[1:] for x in self.files if x != '/']
+
+
+
+def readlink(self, path):
+    return self.data[path]
+
+
+
+def removexattr(self, path, name):
+    attrs = self.files[path].get('attrs', {})
+    try:
+        del attrs[name]
+    except KeyError:
+        pass        # Should return ENOATTR
+
+
+
+def rename(self, old, new):
+    self.files[new] = self.files.pop(old)
+
+
+
+def rmdir(self, path):
+    self.files.pop(path)
+    self.files['/']['st_nlink'] -= 1
+
+
+
+def setxattr(self, path, name, value, options, position=0):
+    # Ignore options
+    attrs = self.files[path].setdefault('attrs', {})
+    attrs[name] = value
+
+
+
+def statfs(self, path):
+    return dict(f_bsize=512, f_blocks=4096, f_bavail=2048)
+
+
+    
+def symlink(self, target, source):
+    self.files[target] = dict(st_mode=(S_IFLNK | 0o777), st_nlink=1,
+                              st_size=len(source))
+    self.data[target] = source
+
+
+
+def truncate(self, path, length, fh=None):
+    self.data[path] = self.data[path][:length]
+    self.files[path]['st_size'] = length
+
+
+
+def unlink(self, path):
+    self.files.pop(path)
+
+
+
+def utimens(self, path, times=None):
+    now = time()
+    atime, mtime = times if times else (now, now)
+    self.files[path]['st_atime'] = atime
+    self.files[path]['st_mtime'] = mtime
+
+
+    
+def write(self, path, data, offset, fh):
+    self.data[path] = self.data[path][:offset] + data
+    self.files[path]['st_size'] = len(self.data[path])
+    return len(data)
